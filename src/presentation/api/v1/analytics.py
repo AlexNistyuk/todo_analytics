78ff689c1868a496adabebe28d5dc22cddee67bd
NistsiukAlexandr <nistsiuk.aliaksandr@pynest.io>
2024-02-06 10:59:54 +0300

    feat: change permissions

--- src/presentation/api/v1/analytics.py
+++ src/presentation/api/v1/analytics.py
@@ -3,21 +3,29 @@ from starlette.status import HTTP_200_OK
 
 from application.use_cases.analytics import AnalyticsUseCase
 from domain.entities.analytics import AnalyticsRetrieve
-from domain.permissions.users import IsAdmin, IsAdminOrIsOwner
+from domain.permissions.users import IsAdminOrIsOwner
 from domain.utils.period import Period
 
 router = APIRouter()
 
 
 @router.get("/{user_id}", response_model=AnalyticsRetrieve, status_code=HTTP_200_OK)
-async def get_all_actions(user_id: int, period: Period, permission=Depends(IsAdmin())):
+async def get_all_user_actions(
+    user_id: int, period: Period, permission=Depends(IsAdminOrIsOwner())
+):
+    await permission.check_object_permission(user_id)
+
     return await AnalyticsUseCase().get_all_actions(user_id, period)
 
 
 @router.get(
     "/{user_id}/tasks", response_model=AnalyticsRetrieve, status_code=HTTP_200_OK
 )
-async def get_all_tasks(user_id: int, period: Period, permission=Depends(IsAdmin())):
+async def get_all_user_tasks(
+    user_id: int, period: Period, permission=Depends(IsAdminOrIsOwner())
+):
+    await permission.check_object_permission(user_id)
+
     return await AnalyticsUseCase().get_tasks(user_id, period)
 
 
@@ -61,7 +69,11 @@ async def get_retrieved_tasks(
 @router.get(
     "/{user_id}/sheets", response_model=AnalyticsRetrieve, status_code=HTTP_200_OK
 )
-async def get_all_sheets(user_id: int, period: Period, permission=Depends(IsAdmin())):
+async def get_all_user_sheets(
+    user_id: int, period: Period, permission=Depends(IsAdminOrIsOwner())
+):
+    await permission.check_object_permission(user_id)
+
     return await AnalyticsUseCase().get_sheets(user_id, period)
 
 
